ca242004033c68228ab2194d6fc18234b68f13aa
Aidil <th3ygen@gmail.com>
2021-12-10 23:03:54 +0800

    item router, api route

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,4 +1,4 @@
-const Item = require('../Item');
+const Item = require('models/Item');
 
 module.exports = {
     // GET
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app = express();
 
 (async () => {
     // connect to the mongodb
-    await require('./service/mongodb.service').connect();
+    await require('service/mongodb.service').connect();
 
     app.get('/test', (req, res) => {
         res.send('Hello World');
     });
+
+    app.use('/api', require('routers'));
     
     app.listen(8080, () => {
         console.log('Server is running on port 8080');
