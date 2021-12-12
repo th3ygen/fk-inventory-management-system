3968abd3861ae95eae319fdc32ea37b974877124
Aidil <th3ygen@gmail.com>
2021-12-12 22:37:05 +0800

    page manage initial

---
+++ react/src/page/common/inventory/Manage.jsx
@@ -0,0 +1,11 @@
+import styles from 'styles/common/inventory/Manage.module.scss';
+
+function ManageInventory() {
+    return (
+        <div className={styles.container}>
+            <h1>Manage Inventory</h1>
+        </div>
+    )
+}
+
+export default ManageInventory;
