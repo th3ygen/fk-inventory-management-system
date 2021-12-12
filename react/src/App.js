9e9e7aaefe4e78fd952494c265d4f6c3a54bd691
Haziq Danial <haziqdanial031@gmail.com>
2021-12-12 23:01:13 +0800

    changed app.js

--- react/src/App.js
+++ react/src/App.js
@@ -2,26 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 	return (
 		<div className="App">
 			<Routes>
@@ -35,7 +15,6 @@ function App() {
 			</Routes>
 		</div>
 	);
->>>>>>> 0d43f40 (nav link click area fix)
 }
 
 export default App;
