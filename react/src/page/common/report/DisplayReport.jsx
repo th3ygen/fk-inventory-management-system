058a37548a350804ca1c0958cb6a63560a33db79
Jasmine <jasminepriscillapk@gmail.comm>
2021-12-19 22:08:40 +0800

    displayReport init

---
+++ react/src/page/common/report/DisplayReport.jsx
@@ -0,0 +1,16 @@
+import styles from "styles/common/report/DisplayReport.module.scss"
+function DisplayReport (){
+
+    return (
+        <div className={styles/containter}> 
+            <table>
+                <tr>
+                    <th>ID</th>
+                </tr>
+            </table>
+
+        </div>
+    )
+}
+
+export default DisplayReport 
