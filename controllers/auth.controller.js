525c9edf2a82ada432d764f1fbdf7c2f013c2eb6
Aidil <th3ygen@gmail.com>
2021-12-20 20:41:56 +0800

    auto format

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,78 +1,79 @@
-const Account = require('../Account');
+const Account = require("../Account");
 
 module.exports = {
-    // POST
-    login: async function(req, res) {
-        try {
-            const { username, password } = req.body;
+	// POST
+	login: async function (req, res) {
+		try {
+			const { username, password } = req.body;
 
-            const user = await Account.login(username,password);
-            if (user) {
-                res.status(200).json(user);
-            } else {
-                res.status(404).json({
-                    error: 'User not found/ wrong password'
+			const token = await Account.login(username, password);
+			if (token) {
+				res.status(200).json({
+                    token
                 });
-            }
-            
-        } catch(e) {
-            console.log(`[ERROR] ${e}`);
-            res.status(500).json({
-                error: e
-            });
-        }
-    },
-    // POST
-    register: async function(req, res) {
-        try {
-            const { email, username, password, contact } = req.body;
+			} else {
+				res.status(401).json({
+					error: "User not found/ wrong password",
+				});
+			}
+		} catch (e) {
+			console.log(`[ERROR] ${e}`);
+			res.status(500).json({
+				error: e,
+			});
+		}
+	},
+	// POST
+	register: async function (req, res) {
+		try {
+			const { email, username, password, contact } = req.body;
 
-            const user = await Account.register(email, username, password, contact);
-            res.status(200).json(user);
-        } catch(e) {
-            console.log(`[ERROR] ${e}`);
-            res.status(500).json({
-                error: e
-            });
-        }
-    },
-    // POST
-    forgotPW: async function(req, res) {
-        try {
-            const { email, username } = req.body;
+			const user = await Account.register(
+				email,
+				username,
+				password,
+				contact
+			);
+			res.status(200).json(user);
+		} catch (e) {
+			console.log(`[ERROR] ${e}`);
+			res.status(500).json({
+				error: e,
+			});
+		}
+	},
+	// POST
+	forgotPW: async function (req, res) {
+		try {
+			const { email, username } = req.body;
 
-            await Account.forgotPW(email, username);
+			await Account.forgotPW(email, username);
 
-           
-            res.status(200).json({
-                message: 'Request Sent'
-            });
-            
-        } catch(e) {
-            console.log(`[ERROR] ${e}`);
-            res.status(500).json({
-                error: e
-            });
-        }
-    },
-    //  POST
-    updatePW: function(req, res) {
-        try {
-            const { account_ID, password } = req.body;
+			res.status(200).json({
+				message: "Request Sent",
+			});
+		} catch (e) {
+			console.log(`[ERROR] ${e}`);
+			res.status(500).json({
+				error: e,
+			});
+		}
+	},
+	//  POST
+	updatePW: function (req, res) {
+		try {
+			const { account_ID, password } = req.body;
 
-            await Account.forgotPW(account_ID, password);
+			await Account.forgotPW(account_ID, password);
 
-           
-            res.status(200).json({
-                message: 'Password Updated'
-            });
-            
-        } catch(e) {
-            console.log(`[ERROR] ${e}`);
-            res.status(500).json({
-                error: e
-            });
-        }
-   
-    }
-}
+			res.status(200).json({
+				message: "Password Updated",
+			});
+		} catch (e) {
+			console.log(`[ERROR] ${e}`);
+			res.status(500).json({
+				error: e,
+			});
+		}
+	},
+};
