8c1f106d6efed6ef717563e388355cf009b10581
Jasmine <jasminepriscillapk@gmail.comm>
2021-12-21 18:44:58 +0800

    ForgotPW UI

--- react/src/page/common/ForgotPassword.jsx
+++ react/src/page/common/ForgotPassword.jsx
@@ -2,20 +2,24 @@ import styles from "styles/common/ForgotPassword.module.scss";
 function ForgotPassword() {
 	return (
 		<div className={styles.container}>
-			<div>
-				<div>
-					<div> Enter new password</div>
+			<div className={styles.whitecontainer}>
+				<div className={styles.title}>
+					<div> Enter New Password</div>
 				</div>
 
-				<form>
+				<form className={styles.formPassword}>
 					<div>
-						<input type="text" id="" placeholder="Username"></input>
+						<input type="text" placeholder="Email"></input>
 					</div>
 
 					<div>
-						<input type="password" id="" placeholder="Password" />
+						<input type="password" placeholder="New Password" />
 					</div>
-					<a href="login.html">Update</a>
+                    
+					<div>
+						<input type="password" placeholder="Repeat Password" />
+					</div>
+					<div className={styles.button} href="login.html">Request</div>
 				</form>
 			</div>
 		</div>
