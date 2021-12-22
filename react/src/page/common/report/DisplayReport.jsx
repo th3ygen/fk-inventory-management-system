d33be9fead0b23f4e2faa78137cc7779bb59e2b6
Aidil <th3ygen@gmail.com>
2021-12-22 19:49:24 +0800

    updated DisplayReport page

--- react/src/page/common/report/DisplayReport.jsx
+++ react/src/page/common/report/DisplayReport.jsx
@@ -1,4 +1,6 @@
+// components
 import Table from "components/Table.component";
+import TopList from "components/TopList.component";
 
 import styles from "styles/common/report/DisplayReport.module.scss";
 
@@ -38,13 +40,46 @@ function DisplayReport() {
 			["Item 1", "Item 2", "Item 3", "Item 4"],
 			["Item 1", "Item 2", "Item 3", "Item 4"],
 		],
-		colWidthPercent: ["60%", "10%", "10%", "20%"],
+		colWidthPercent: ["30%", "10%", "10%", "10%"],
         centered: [false, true, true, true],
 	};
 
+	const topSold = [
+		{
+			'Name': 'Item 1',
+			'Total Sold': '10',
+		},
+		{
+			'Name': 'Item 2',
+			'Total Sold': '20',
+		},
+		{
+			'Name': 'Item 3',
+			'Total Sold': '30',
+		},
+		{
+			'Name': 'Item 4',
+			'Total Sold': '40',
+		},
+		{
+			'Name': 'Item 5',
+			'Total Sold': '50',
+		},
+	];
+
+	// sort the topSold array by quantity
+	topSold.sort((a, b) => {
+		return b.quantity - a.quantity;
+	});
+
 	return (
-		<div className={styles.containter}>
-			<Table title="Items sold" data={itemsData} perPage={2}></Table>
+		<div className={styles.container}>
+			<div className={styles.itemsSoldTable}>
+				<Table title="Items sold" data={itemsData} />
+			</div>
+			<div className={styles.topSoldList}>
+				<TopList title="Hot items" data={topSold} />
+			</div>
 		</div>
 	);
 }
