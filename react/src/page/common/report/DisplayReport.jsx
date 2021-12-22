0af0505c015258cb4e3daab007be1e4ceaf16e2b
Aidil <th3ygen@gmail.com>
2021-12-22 19:10:20 +0800

    added sample use for table component

--- react/src/page/common/report/DisplayReport.jsx
+++ react/src/page/common/report/DisplayReport.jsx
@@ -1,16 +1,52 @@
-import styles from "styles/common/report/DisplayReport.module.scss"
-function DisplayReport (){
+import Table from "components/Table.component";
 
-    return (
-        <div className={styles/containter}> 
-            <table>
-                <tr>
-                    <th>ID</th>
-                </tr>
-            </table>
+import styles from "styles/common/report/DisplayReport.module.scss";
 
-        </div>
-    )
+function DisplayReport() {
+	const itemsData = {
+		header: ["Header 1", "Header 2", "Header 3", "Header 4"],
+		items: [
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Test", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+			["Item 1", "Item 2", "Item 3", "Item 4"],
+		],
+		colWidthPercent: ["60%", "10%", "10%", "20%"],
+        centered: [false, true, true, true],
+	};
+
+	return (
+		<div className={styles.containter}>
+			<Table title="Items sold" data={itemsData} perPage={2}></Table>
+		</div>
+	);
 }
 
-export default DisplayReport 
+export default DisplayReport;
