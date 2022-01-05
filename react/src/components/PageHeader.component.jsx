a9e14dcd257e1e8ff238ddf43ec4ffb004975d42
Aidil <th3ygen@gmail.com>
2022-01-06 01:27:00 +0800

    updated PageHeader: added nav buttons

--- react/src/components/PageHeader.component.jsx
+++ react/src/components/PageHeader.component.jsx
@@ -1,16 +1,30 @@
+import * as ReactIcons from "react-icons/fa";
+
 import styles from "styles/component/PageHeader.module.scss";
 
 function PageHeader(props) {
-    return (
-        <div className={styles.container}>
-            <div className={styles.title}>
-                {props.title || "Unknown"}
-            </div>
-            <div className={styles.brief}>
-                {props.brief || "Unknown"}
-            </div>
-        </div>
-    )
+	const Icon = ({ name }) => {
+		const TagName = ReactIcons[name];
+		return !!TagName ? <TagName /> : <p>{name}</p>;
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.text}>
+				<div className={styles.title}>{props.title || "Unknown"}</div>
+				<div className={styles.brief}>{props.brief || "Unknown"}</div>
+			</div>
+			<div className={styles.navs}>
+				{props.navs &&
+					props.navs.map((nav, i) => (
+						<div key={i} className={styles.nav}>
+							<Icon name={nav.icon} />
+							<span>{nav.name}</span>
+						</div>
+					))}
+			</div>
+		</div>
+	);
 }
 
 export default PageHeader;
