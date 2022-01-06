1acfb24a7bd38100f3599fe19daa1ad0cec9da85
Aidil <th3ygen@gmail.com>
2022-01-06 12:04:41 +0800

    useLocation

--- react/src/page/common/inventory/AddItem.jsx
+++ react/src/page/common/inventory/AddItem.jsx
@@ -3,14 +3,17 @@
     TODO client validation
     TODO fetch data from server
 */
+import { useLocation } from "react-router-dom";
 
 import styles from "styles/common/inventory/AddItem.module.scss";
 
 function AddItem () {
+    const location = useLocation();
+
     return (
         <div className={styles.container}>
             <div className={styles.form}>
-            
+                {location.state && location.state.label }
             </div>
         </div>
     );
