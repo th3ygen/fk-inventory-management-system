da9f1b090f25bb70812fc105806a79b92c1afdd5
Aidil <th3ygen@gmail.com>
2022-01-06 15:43:39 +0800

    UpdateItem init

--- react/src/page/common/inventory/UpdateItem.jsx
+++ react/src/page/common/inventory/UpdateItem.jsx
@@ -1,21 +1,30 @@
 /* 
     TODO Update item form
+    TODO fetch item details
     TODO client validation
 */
 import { useLocation } from "react-router-dom";
 
+import PageHeader from "components/PageHeader.component";
+
 import styles from "styles/common/inventory/UpdateItem.module.scss";
+import { useEffect } from "react";
 
-function UpdateItem () {
-    const location = useLocation();
+function UpdateItem() {
+	const location = useLocation();
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.form}>
-                {location.state && location.state.item }
-            </div>
-        </div>
-    );
+    useEffect(() => {
+        console.log(location.state);
+    }, []);
+
+	return (
+		<div className={styles.container}>
+			<PageHeader title="Update Item" brief="Update an item" />
+			<div className={styles.form}>
+				
+			</div>
+		</div>
+	);
 }
 
 export default UpdateItem;
