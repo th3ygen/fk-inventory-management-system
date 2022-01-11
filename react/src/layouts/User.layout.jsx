fd4c2b6484f224647c537cce832ee3496c93be43
Haziq Danial <haziqdanial031@gmail.com>
2022-01-11 23:07:35 +0800

    updated changes - added manage,edit,register

--- react/src/layouts/User.layout.jsx
+++ react/src/layouts/User.layout.jsx
@@ -1,33 +1,46 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import styles from 'styles/layout/User.module.scss';
+import styles from "styles/layout/User.module.scss";
 
-import Navbar from 'components/Navbar.component';
-import Topbar from 'components/Topbar.component';
+import Navbar from "components/Navbar.component";
+import Topbar from "components/Topbar.component";
 
-export default function UserLayout() {
-    const paths = [
-        {
-            path: '/user',
-            name: 'User',
-        },
-        {  
-            path: '/user/tests',
-            name: 'Test',
-        },
-        {  
-            path: '/user/manage',
-            name: 'Manage',
-        }
-    ];
+export default function UserLayout(props) {
+	const paths = [
+		{
+			path: "/user/inventory",
+			name: "Inventory",
+			icon: "FaBox",
+		},
+		{
+			path: "/user/orders",
+			name: "Orders",
+			icon: "FaReceipt",
+		},
+		{
+			path: "/user/vendors",
+			name: "Vendors",
+			icon: "FaShoppingBag",
+		},
+		{
+			path: "/user/report",
+			name: "Report",
+			icon: "FaClipboardList",
+		},
+		{
+			path: "/user/inbox",
+			name: "Inbox",
+			icon: "FaInbox",
+		},
+	];
 
-    return (
-        <div>
-            <Topbar />
-            <Navbar paths={ paths }/>
-            <div className={ styles.content }>
-                <Outlet />
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<Topbar />
+			<Navbar paths={paths} />
+			<div className={styles.content}>
+				<Outlet />
+			</div>
+		</div>
+	);
 }
