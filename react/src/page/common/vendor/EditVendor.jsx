3033fa3e35bb0abf6ba00ed6caaf76eafd884c14
Haziq Danial <haziqdanial031@gmail.com>
2022-01-18 00:36:34 +0800

    updated manage vendor front end

--- react/src/page/common/vendor/EditVendor.jsx
+++ react/src/page/common/vendor/EditVendor.jsx
@@ -1,7 +1,104 @@
+import { useState, useRef, useEffect } from 'react';
+import { FaFileImport, FaTrashAlt } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+
+import PageHeader from "components/PageHeader.component";
+
+import styles from 'styles/common/vendor/RegisterVendor.module.scss';
+
 function EditVendor() {
+    const [items, setItems] = useState([]);
+    
+    const companyName = useRef('');
+    const brandName = useRef('');
+    const contactNo = useRef('');
+    const addressInput = useRef('');
+    const email = useRef('');
+    const picName = useRef('');
+    const picContact = useRef('');
+
+    const addVendor = async () => {
+        
+    }
+
     return (
-        <div>
-            <h1>Hello world!</h1>
+        <div className={styles.container}>
+            <PageHeader
+				title="Edit Vendor"
+				brief="Edit existing vendor"
+				navs={[
+					{
+						icon: "FaReply",
+						name: "Manage vendor",
+						path: "/user/vendors",
+					},
+				]}
+			/>
+
+            <form className={styles.form}>
+                <div className={`${styles.col} ${styles.half}`}>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Company Name</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={companyName}
+                        />
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Brand Name</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={brandName}
+                        />
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Contact Number</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={contactNo}
+                        />
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Address</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={addressInput}
+                        />
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Email</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={email}
+                        />
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Person In Charge</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={picName}
+                        />
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.label}>Person In Charge (Number)</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            ref={picContact}
+                        />
+                    </div>
+                    <div className={styles.button} onClick={addVendor}>
+                        <FaFileImport />
+                        Add
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
--- react/src/page/common/vendor/ManageVendor.jsx
+++ react/src/page/common/vendor/ManageVendor.jsx
@@ -1,10 +1,70 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { FaEdit, FaTrashAlt } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+import styles from 'styles/common/vendor/ManageVendor.module.scss';
+
+import Table from 'components/Table.component';
+import PageHeader from 'components/PageHeader.component';
 
 function ManageVendor() {
+    const [items, setItems] = useState([]);
+    const navigate = useNavigate();
+
+    const vendorData = {
+        header: ["Company Name", "brand", "contact", "email", "Person in Charge", "Contact Number"],
+        items: [
+            [1,"company 1", "brand 1", "contact 1", "email 1", "pic 1", "contact 1"],
+            [2,"company 2", "brand 2", "contact 2", "email 2", "pic 2", "contact 2"]
+        ],
+        actions: [
+            {
+                icon: "FaEdit",
+                callback: (n) => {
+					navigate("/user/vendors/edit");
+				},
+                tooltip: "Edit",
+            },
+            {
+                icon: "FaTrashAlt",
+                callback: (n) => {
+					console.log('deleting', n);
+				},
+                tooltip: "Delete",
+            }
+        ],
+        colWidthPercent: ["20%", "10%", "10%", "10%", "20%", "15%"],
+        centered: [false, true, true, true, true, true],
+    };
+
+    useEffect(() => {
+        setItems(vendorData.items);
+    });
     return (
-        <div>
-            <h1>Hello Vendor</h1>
-            <p>test</p>
+        <div className={styles.container}>
+            <PageHeader
+                title="Manage Vendor"
+                brief="This is the page to manage vendor"
+                navs={[
+                    {
+                        icon: "FaReply",
+                        name: "Issue new Vendor",
+                        path: "/user/vendors/add",
+                    },
+                    
+                ]}
+
+            />
+            <div className={styles.vendorTable}>
+                <Table
+                    title="Vendors"
+                    headers={vendorData.header}
+                    items={vendorData.items}
+                    centered={vendorData.centered}
+                    colWidthPercent={vendorData.colWidthPercent}
+                    actions={vendorData.actions}
+                />
+            </div>
         </div>
     )
 }
