a0f984528d251f885244eb10fa51c6f760eec8cd
Aidil <th3ygen@gmail.com>
2022-01-18 14:44:19 +0800

    fix3: TopList

--- react/src/components/TopList.component.jsx
+++ react/src/components/TopList.component.jsx
@@ -1,15 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import FolderCard from "components/FolderCard";
 
 import styles from "styles/component/TopList.module.scss";
 
 function TopList(props) {
-	const { title, data } = props;
+	const [data, setData] = useState([]);
+	const [headers, setHeaders] = useState([]);
+
+	const { title } = props;
 
 	useEffect(() => {
-		if (props.data) {
-			console.log(data);
+		if (props.data && props.data.length > 0) {
+			let h = Object.keys(props.data[0]);
+			let d = props.data.map((item) => Object.values(item));
+
+			setHeaders(h);
+			setData(d);
 		}
 	}, [props.data]);
 
@@ -17,7 +24,7 @@ function TopList(props) {
 		<FolderCard className={styles.container} title={title}>
 			<div className={styles.list}>
 				<div className={styles.header}>
-					{data && Object.keys(data[0]).map((item, index) => {
+					{headers.map((item, index) => {
 						return (
 							<div key={index} className={styles.item}>
 								{item}
@@ -26,22 +33,17 @@ function TopList(props) {
 					})}
 				</div>
 				<div className={styles.body}>
-					{data && data.map((item, index) => {
-						return (
-							<div key={index} className={styles.item}>
-								{Object.values(item).map((value, index) => {
-									return (
-										<div
-											key={index}
-											className={styles.value}
-										>
-											{value}
-										</div>
-									);
-								})}
-							</div>
-						);
-					})}
+					{data.map((item, index) => (
+						<div key={index} className={styles.item}>
+							{item.map((value, index) => {
+								return (
+									<div key={index} className={styles.value}>
+										{value}
+									</div>
+								);
+							})}
+						</div>
+					))}
 				</div>
 			</div>
 		</FolderCard>
