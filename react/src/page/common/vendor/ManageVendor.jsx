986eb38be26d2f04e6794c637fec109fd226f772
Haziq Danial <haziqdanial031@gmail.com>
2022-01-25 01:14:12 +0800

    fix delete front end

--- react/src/page/common/vendor/ManageVendor.jsx
+++ react/src/page/common/vendor/ManageVendor.jsx
@@ -74,8 +74,7 @@ function ManageVendor() {
 
 		if (request.status === 200) {
 			await swal("Deleted", "Vendor Succesfully Deleted!", "error");
-			const item = items.find(i => i[0] === id);
-
+            setItems(items.filter((i) => i[0] !== id));
 		} else {
 			console.log(id, request);
 			alertify.notify('Error deleting vendor', 'error');
